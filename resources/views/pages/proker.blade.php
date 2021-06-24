ab7ddfd649388a7ca37f5b38bc12ca68575884d3
TobiasRenal <1872015@maranatha.ac.id>
2021-06-25 02:39:52 +0700

    alert

--- resources/views/pages/proker.blade.php
+++ resources/views/pages/proker.blade.php
@@ -150,6 +150,15 @@
       </div>
     </nav>
     <!-- Header -->
+    @if(session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <span class="alert-icon"><i class="ni ni-like-2"></i></span>
+      <span class="alert-text"><strong>Success!</strong> Data berhasil ditambahkan!</span>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    @endif
     <!-- Header -->
     <div class="header bg-primary pb-6">
       <div class="container-fluid">
