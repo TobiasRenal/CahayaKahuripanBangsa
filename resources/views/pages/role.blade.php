ff74ebf66fadecc3edbd4a469a3010f23a8c0192
TobiasRenal <1872015@maranatha.ac.id>
2021-06-24 07:54:27 +0700

    minor fix

--- resources/views/pages/role.blade.php
+++ resources/views/pages/role.blade.php
@@ -328,7 +328,7 @@
         <div class="header-body">
           <div class="row align-items-center py-4">
             <div class="col-lg-6 col-7">
-              <h1 class="h1 text-white d-inline-block mb-0">Organisasi</h1>
+              <h1 class="h1 text-white d-inline-block mb-0">Role Pengurus</h1>
             </div>
             <div class="col-lg-6 col-5 text-right">
             <!-- Add DATA -->
